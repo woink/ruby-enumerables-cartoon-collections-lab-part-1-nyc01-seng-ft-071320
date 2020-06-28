7f3a481be4847c2b4038dab2f403c47b3dbbc754
Ward Price <wardprice@gmail.com>
2020-06-28 20:34:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,5 +9,5 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index { |i, | puts x }
+  array.each_with_index { |item, index| puts "#{index + 1}. #{item}" }
 end
